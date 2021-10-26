c811cff8bb3051bb96dc2e6c35caa7d56af24830
Quan Nguyễn <quanneprvt@gmail.com>
2021-10-26 08:10:38 +0700

    add app view test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,12 @@
+import { shallow, ShallowWrapper } from 'enzyme';
 import React from 'react';
 import App from './App';
 
-test('renders learn react link', () => {
-  
+describe("APP View", () => {
+  let wrapper: ShallowWrapper;
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
+  it("should render correctly", () => expect(wrapper).toMatchSnapshot());
 });
